785cb8fa5b3f0fbb5fffb4cc15fb6619e11313ad
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-10-17 11:13:22 +0600

    toggle button clicked and change state

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import "./Country.css"
 const Country = ({country}) => {
-    console.log(country)
-
     const {name, flags, region, status} = country ;
+
+    const [visited, setVisited]= useState(false);
+
+    const handleVisit = ()=>{
+        setVisited(!visited)
+    }
     return (
         <div className="country">
             <h2>Name : {name.common}</h2>
@@ -10,6 +15,9 @@ const Country = ({country}) => {
             <h3>Region : {region}</h3>
             <h3>Status : {status}</h3>
             <img src={flags?.png} alt="" />
+            <br /> <br />
+            <button onClick={handleVisit}>{visited ? "visited" : "Going"}</button>
+            {visited ? "I have visited" : "Not visited"}
             
         </div>
     );
